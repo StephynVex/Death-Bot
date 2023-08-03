2f4fe28ae398ec8fe954b2d264d3f4f4cc5a49ed
Alexito <103476365+Alexito-Hub@users.noreply.github.com>
2023-08-03 15:27:10 -0500

    Update update.js

--- group-participants/update.js
+++ group-participants/update.js
@@ -1,1 +1,50 @@
-module.exports = async()
+
+
+module.exports = async(sock, v, message) => {
+    try {
+        const groupId = update.id;
+        const participants = update.participants;
+        const action = update.action;
+
+        const groupMetadata = await sock.groupMetadata(groupId);
+        const groupName = groupMetadata.subject;
+        
+        for (const participant of participants) {
+            const user = participant.split('@')[0];
+            console.log(`participant update: ${participant}, action: ${action}, user: ${user}, group: ${groupId}`);
+            if (action === 'add') {
+                sock.sendMessage(groupId, {
+                    text: `¡Hola @${user}! Bienvenido/a al grupo. ¡Esperamos que te diviertas y disfrutes tu estancia aquí! 🎉`,
+                    contextInfo: {
+                        mentionedJid: [ participant ],
+                        externalAdReply: {
+                            title: `DEATH BOT`,
+                            body: ``,
+                            sourceUrl: '',
+                            renderLargerThumbnail: true, 
+                            mediaType: 1, 
+                            thumbnailUrl: ''
+                        }
+                    }
+                });
+            } else if (action === 'remove') {
+                sock.sendMessage(groupId, {
+                    text: `Adiós @${user}. Esperamos que hayas tenido una buena experiencia en el grupo. ¡Te echaremos de menos! 👋`,
+                    contextInfo: {
+                        mentionedJid: [ participant ],
+                        externalAdReply: {
+                            title: `DEATH BOT`,
+                            body: ``,
+                            sourceUrl: '',
+                            renderLargerThumbnail: true, 
+                            mediaType: 1, 
+                            thumbnailUrl: ''
+                        },
+                    }
+                });
+            }
+        }
+    } catch (e) {
+        console.log(`Error : ${e}`)
+    }
+          }
